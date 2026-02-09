345b4f148a46a33a57169c31c3bae1d5cda62a06
teggar4ar <teggarar123@gmail.com>
2026-02-09 19:32:56 +0700

    fix: add Alpine.js collapse plugin for attendance recording

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,12 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Alpine.js Collapse Plugin
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+
+Alpine.plugin(collapse);
+
+window.Alpine = Alpine;
+Alpine.start();
